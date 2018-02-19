f96d1c823bc0cd9df059028d7709c445b2c1587c
Christian Oberdörfer <oberdoerfer@quantum.qa>
2018-02-19 22:14:46 +0100

    Added LogEntryTests

---
+++ QLog/QLogTests/LogEntryTests.swift
@@ -0,0 +1,32 @@
+//
+//  LogEntryTests.swift
+//  QLogTests
+//
+//  Created by Christian Oberdörfer on 21.06.17.
+//  Copyright © 2017 Quantum. All rights reserved.
+//
+
+@testable import QLog
+import XCTest
+
+class LogEntryTests: XCTestCase {
+
+    func testMetaText() {
+        // 1. Arrange
+        let dateString = "04:07:11"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        let date = dateFormatter.date(from: dateString)!
+        let file = "File.dat"
+        let path = "Directory/File.dat"
+        let function = "Function"
+        let line = 0
+        let logLevel: LogLevel = .error
+        let text = "Text"
+        let logEntry = LogEntry(date: date, file: path, function: function, line: line, logLevel: logLevel, text: text)
+
+        // 3. Assertdate
+        XCTAssertEqual(logEntry.metaText, "\(dateString): \(file):\(line) \(function): ")
+    }
+
+}
