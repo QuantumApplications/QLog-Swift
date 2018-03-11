2af103bfd9d6b3981abd67883407e8db57cf1942
Christian Oberdörfer <oberdoerfer@quantum.qa>
2018-03-11 23:22:05 +0100

    Added QLogTests

---
+++ QLog/QLogTests/QLogTests.swift
@@ -0,0 +1,39 @@
+//
+//  QLogTests.swift
+//  QLogTests
+//
+//  Created by Christian Oberdörfer on 11.03.18.
+//  Copyright © 2018 Quantum. All rights reserved.
+//
+
+@testable import QLog
+import Cuckoo
+import XCTest
+
+class QLogTests: XCTestCase {
+
+    func testGetCalls() {
+        // 1. Arrange
+        class TestLogger: Logger {
+
+            public var logLevel: LogLevel = .highlight
+
+            var logged = false
+
+            func doLog(_ logEntry: LogEntry) {
+                self.logged = true
+            }
+
+        }
+
+        let testLogger = TestLogger()
+        QLog.loggers = [testLogger]
+
+        // 2. Action
+        QLogHighlight("test")
+
+        // 3. Assert
+        XCTAssertTrue(testLogger.logged)
+    }
+
+}
