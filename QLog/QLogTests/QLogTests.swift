0f6ea18a58a5ca8ff6ed6b8e29508039fcfee723
Christian Oberdörfer <oberdoerfer@quantum.qa>
2018-03-21 23:42:39 +0100

    Updated QLogTests

--- QLog/QLogTests/QLogTests.swift
+++ QLog/QLogTests/QLogTests.swift
@@ -15,16 +15,18 @@ class TestLogger: Logger {
     public var logLevel: LogLevel = .highlight
 
     var logged = false
+    var loggedLevel: LogLevel = .highlight
 
     func doLog(_ logEntry: LogEntry) {
         self.logged = true
+        self.loggedLevel = logEntry.logLevel
     }
 
 }
 
 class QLogTests: XCTestCase {
 
-    func testGetCalls() {
+    func testQLogHighlight() {
         // 1. Arrange
         let testLogger = TestLogger()
         QLog.loggers = [testLogger]
@@ -34,6 +36,59 @@ class QLogTests: XCTestCase {
 
         // 3. Assert
         XCTAssertTrue(testLogger.logged)
+        XCTAssertEqual(testLogger.loggedLevel, LogLevel.highlight)
+    }
+
+    func testQLogDebug() {
+        // 1. Arrange
+        let testLogger = TestLogger()
+        QLog.loggers = [testLogger]
+
+        // 2. Action
+        QLogDebug("test")
+
+        // 3. Assert
+        XCTAssertTrue(testLogger.logged)
+        XCTAssertEqual(testLogger.loggedLevel, LogLevel.debug)
+    }
+
+    func testQLogInfo() {
+        // 1. Arrange
+        let testLogger = TestLogger()
+        QLog.loggers = [testLogger]
+
+        // 2. Action
+        QLogInfo("test")
+
+        // 3. Assert
+        XCTAssertTrue(testLogger.logged)
+        XCTAssertEqual(testLogger.loggedLevel, LogLevel.info)
+    }
+
+    func testQLogWarning() {
+        // 1. Arrange
+        let testLogger = TestLogger()
+        QLog.loggers = [testLogger]
+
+        // 2. Action
+        QLogWarning("test")
+
+        // 3. Assert
+        XCTAssertTrue(testLogger.logged)
+        XCTAssertEqual(testLogger.loggedLevel, LogLevel.warning)
+    }
+
+    func testQLogError() {
+        // 1. Arrange
+        let testLogger = TestLogger()
+        QLog.loggers = [testLogger]
+
+        // 2. Action
+        QLogError("test")
+
+        // 3. Assert
+        XCTAssertTrue(testLogger.logged)
+        XCTAssertEqual(testLogger.loggedLevel, LogLevel.error)
     }
 
 }
