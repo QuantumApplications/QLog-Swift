10fbedeba55ae8d440110d0ef7e0a286b8ef13f8
Christian Oberdörfer <oberdoerfer@quantum.qa>
2019-02-13 15:48:09 +0100

    Updated LoggerTests

--- QLog/QLogTests/LoggerTests.swift
+++ QLog/QLogTests/LoggerTests.swift
@@ -62,4 +62,80 @@ class LoggerTests: XCTestCase {
         XCTAssertFalse(logger.logged)
     }
 
+    func testEquals() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .error, text: "")
+        let logEntry2 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertTrue(logEntry1 == logEntry2)
+        XCTAssertEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentDate() {
+        // 1. Arrange
+        let logEntry1 = LogEntry(date: Date(), file: "", function: "", line: 0, logLevel: .error, text: "")
+        let logEntry2 = LogEntry(date: Date(), file: "", function: "", line: 0, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentFile() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "1", function: "", line: 0, logLevel: .error, text: "")
+        let logEntry2 = LogEntry(date: date, file: "2", function: "", line: 0, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentFunction() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "", function: "1", line: 0, logLevel: .error, text: "")
+        let logEntry2 = LogEntry(date: date, file: "", function: "2", line: 0, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentLine() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "", function: "", line: 1, logLevel: .error, text: "")
+        let logEntry2 = LogEntry(date: date, file: "", function: "", line: 2, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentLogLevel() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .warning, text: "")
+        let logEntry2 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .error, text: "")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
+    func testEqualsDifferentText() {
+        // 1. Arrange
+        let date = Date()
+        let logEntry1 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .error, text: "1")
+        let logEntry2 = LogEntry(date: date, file: "", function: "", line: 0, logLevel: .error, text: "2")
+
+        // 3. Assert
+        XCTAssertFalse(logEntry1 == logEntry2)
+        XCTAssertNotEqual(logEntry1, logEntry2)
+    }
+
 }
