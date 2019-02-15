6c7962f5a16046988863a910e0f71e956350138e
Christian Oberdörfer <oberdoerfer@quantum.qa>
2019-02-15 21:30:21 +0100

    Updated LogsTableViewControllerTests

--- QLog/QLogTests/LogsTableViewControllerTests.swift
+++ QLog/QLogTests/LogsTableViewControllerTests.swift
@@ -12,6 +12,8 @@ import XCTest
 
 class LogsTableViewControllerTests: XCTestCase {
 
+    // MARK: - Table view data source
+
     func testNumberOfSections() {
         // 1. Arrange
         let logsTableViewController = LogsTableViewController()
@@ -26,6 +28,14 @@ class LogsTableViewControllerTests: XCTestCase {
 
         // 3. Assert
         XCTAssertEqual(logsTableViewController.tableView(logsTableViewController.tableView, numberOfRowsInSection: 0), 0)
+    }
+
+    func testHeightForRowAt() {
+        // 1. Arrange
+        let logsTableViewController = LogsTableViewController()
+
+        // 3. Assert
+        XCTAssertEqual(logsTableViewController.tableView(logsTableViewController.tableView, heightForRowAt: IndexPath(row: 0, section: 0)), 44.0)
     }
 
 }
